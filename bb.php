a604708ee0f7027bc46d1ae64949806364ba0335
subrotoice <subroto.iu@gmail.com>
2022-08-11 12:29:16 +0600

    ..f

--- bb.php
+++ bb.php
@@ -2,4 +2,20 @@
 // VS CODE
 Now office desktop
 Funny
+Ok fff// Laptop bb
+// VS CODE
+Now office desktop
+Funny
+Ok fff// Laptop bb
+// VS CODE
+Now office desktop
+Funny
+Ok fff// Laptop bb
+// VS CODE
+Now office desktop
+Funny
+Ok fff// Laptop bb
+// VS CODE
+Now office desktop
+Funny
 Ok fff
